7074ecf56a1735eed9203513b616213e4ab3d17a
srios007 <sriosv@correo.udistrital.edu.co>
2022-12-14 16:19:59 -0500

    [ADD] see residents

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:proyecto_bases_final_consuelo/app/models/models.dart';
 import 'package:proyecto_bases_final_consuelo/app/modules/home/controllers/home_controller.dart';
 import 'package:proyecto_bases_final_consuelo/app/utils/utils.dart';
 import 'package:proyecto_bases_final_consuelo/app/widgets/widgets.dart';
@@ -23,17 +24,44 @@ class HomeView extends GetView<HomeController> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       const TopBarContents(),
-                      const SizedBox(height: 30),
+                      const Divider(),
+                      Row(
+                        children: [
+                          ResidentListTitleItem(
+                            text: 'Bloque',
+                            isTitle: true,
+                          ),
+                          ResidentListTitleItem(
+                            text: 'Apartamento',
+                            isTitle: true,
+                          ),
+                          ResidentListTitleItem(
+                            text: 'Tipo identificación',
+                            isTitle: true,
+                          ),
+                          ResidentListTitleItem(
+                            text: 'Identificación',
+                            isTitle: true,
+                          ),
+                          ResidentListTitleItem(
+                            text: 'Nombre completo',
+                            isTitle: true,
+                          ),
+                          ResidentListTitleItem(
+                            text: 'Género',
+                            isTitle: true,
+                          ),
+                        ],
+                      ),
+                      const Divider(),
                       SizedBox(
-                        height: 80.0 * controller.residentsList.length,
+                        height: 101.0 * controller.residentsList.length,
                         width: Get.width,
                         child: ListView.builder(
                           itemCount: controller.residentsList.length,
                           itemBuilder: (context, index) {
-                            final resident = controller.residentsList[index];
                             return ResidentListTitle(
-                              controller: controller,
-                              
+                              resident: controller.residentsList[index],
                             );
                           },
                           shrinkWrap: true,
@@ -49,20 +77,65 @@ class HomeView extends GetView<HomeController> {
 }
 
 class ResidentListTitle extends StatelessWidget {
-  const ResidentListTitle({super.key, required this.controller});
-  final HomeController controller;
+  const ResidentListTitle({super.key, required this.resident});
+  final Resident resident;
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-Text('')
-    ],);
+    return Column(
+      children: [
+        Row(
+          children: [
+            ResidentListTitleItem(text: resident.codBloque!),
+            ResidentListTitleItem(text: '${resident.codApartamento!}'),
+            ResidentListTitleItem(text: resident.tipoIdentificacion!),
+            ResidentListTitleItem(text: resident.identificacionPersona!),
+            ResidentListTitleItem(text: resident.nombreCompleto!),
+            ResidentListTitleItem(text: resident.generoPersona!),
+          ],
+        ),
+        const Divider()
+      ],
+    );
   }
 }
+
 class ResidentListTitleItem extends StatelessWidget {
-  const ResidentListTitleItem({super.key});
+  ResidentListTitleItem({
+    super.key,
+    required this.text,
+    this.isTitle = false,
+  });
+  final String text;
+  bool isTitle;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: Get.width / 6,
+      height: 100,
+      child: Center(
+        child: Text(
+          text,
+          style: isTitle
+              ? const TextStyle(
+                  color: Palette.darkBlue,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24)
+              : const TextStyle(
+                  color: Palette.darkBlue,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 18,
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class Divider extends StatelessWidget {
+  const Divider({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(width: Get.width, height: 1, color: Palette.darkBlue);
   }
 }
